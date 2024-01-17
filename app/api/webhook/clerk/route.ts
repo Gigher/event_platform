ff3741814b1f2de6f9e1b6de365e3fef88576cd8
Gigher <gastelo1301@gmail.com>
2024-01-17 21:34:29 +0300

    resolve webhook type problem

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -58,17 +58,17 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        first_name: first_name,
-        last_name: last_name,
-        photo: image_url
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
     const newUser = await createUser(user);
 
-    if (newUser) {
+    if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
